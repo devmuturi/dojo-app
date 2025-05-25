ea5394338bf28181a2a1b7204d5c514126711763
devmuturi <muturinjonge09@gmail.com>
2025-05-25 11:43:59 +0300

    Fetch data using useEffect.
    We created a json-sever and we are using useEffect to fetch that data.(Fake api)

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import BlogList from './components/BlogList'
 import NavBar from './components/NavBar'
 
 export default function Content() {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Learning React', body: 'lorem ipsum...', author: 'njonge', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs)
   }
+
+  // fetch the data using useEffect
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data)
+      })
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={ handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's blogs" />
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's blogs" />}
     </div>
   )
 }
