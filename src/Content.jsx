a663e052749656171fb71cffcdc6da551165b9d1
devmuturi <muturinjonge09@gmail.com>
2025-05-25 12:09:53 +0300

    Handle error messages

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 export default function Content() {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -15,20 +16,31 @@ export default function Content() {
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetch data for that resource');
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data)
         setLoading(false)
+        setError(null)
+      })
+      .catch(err => {
+        setLoading(false)
+        setError(err.message)
       })
   }, []);
 
   return (
     <div>
       <NavBar />
-      {isLoading && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
-      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's blogs" />}
+      <div className="container">
+        {isLoading && <div>Loading...</div>}
+        {error && <div>{ error }</div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+        {blogs && <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title="Mario's blogs" />}
+      </div>
     </div>
   )
 }
